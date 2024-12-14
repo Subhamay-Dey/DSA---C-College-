b038eb2d1452e9842ebbad5bfb4fa1a4b1601c85
Subhamay-Dey <ncdey1966@gmail.com>
2024-12-14 23:29:20 +0530

    Reverse Integer done (C++)✅

---
+++ reverseInteger.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0;
+
+        while(x != 0) {
+            int temp = x % 10;
+            x = x / 10;
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && temp > 7)) {
+                return 0; // Overflow
+            }
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && temp < -8)) {
+                return 0; // Underflow
+            }
+
+            result = result * 10 + temp;
+        }
+        return result;
+    }
+};
